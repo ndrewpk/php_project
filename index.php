abfe3723af17c48087d3942165d963b6deff5efc
TainYanTun <166300382+TainYanTun@users.noreply.github.com>
2025-08-18 14:24:51 +0700

    For andrew

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 echo "tes ";
 $x =5;
 $_isAdmin = true;
-
-
-
-
+echo "Hello, World! This is the index.php file.";
+// Additional code can be added here for further functionality
+echo "\nThis is a simple PHP script.";
+echo "\nYou can modify this file to change the output.";
+echo "\nEnjoy coding in PHP!";
+echo "\nHave a great day!";
+echo "\nThis is the end of the index.php file.";
+echo "\nThank you for using this script.";
 
 ?>
